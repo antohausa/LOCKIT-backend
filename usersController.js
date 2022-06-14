83ea5815e8aa600ab455849e75e09e143aa14c91
antohausa <101648361+antohausa@users.noreply.github.com>
2022-06-14 08:45:22 -0300

    Update usersController.js

--- usersController.js
+++ usersController.js
@@ -1,20 +1,19 @@
 import getAll from './usersService.js'
 
 export const getUsers = async (req, res) => {
-    try{
+   
     console.log("aca tenes los usuarios")
     const all = await getAll();
-    
+    res.send(all)
     //res.send(JSON.stringify({'HOLA':'haaaaaa'}))
     //res.send(all)
    // res.send("CONCHA DE TU HERMANA")
     //req users to service
-    }
-    catch (err){
+    /*catch (err){
         console.log("ERROR! I'm sorry!!!!!!!")
         throw err;
         
-    }
+    }*/
 };
 
 export const createUsers = async (req, res) => {
