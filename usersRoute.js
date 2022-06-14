a81852b4f02ad7d90a2d3a75f08f34a99baa708a
antohausa <101648361+antohausa@users.noreply.github.com>
2022-06-14 08:45:41 -0300

    Update usersRoute.js

--- usersRoute.js
+++ usersRoute.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {getUsers, createUsers} from './usersController.js';
 const router = Router()
 
-router.get('/get', getUsers);
-router.get('/post', createUsers);
+router.get('/getUsers', getUsers);
+router.get('/createUsers', createUsers);
 
 export default router;
