af4ae5a6d50df9d184f5d0aa4ead1c334753b4d7
ANTONELLA HAUSERMAN <46582604@ort>
2022-06-15 17:00:01 -0300

    yaayyyyyyyyyyyyyyyyyy

--- users/services/usersService.js
+++ users/services/usersService.js
@@ -1,23 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
-
-
 import pkg from 'pg';
+
+
+//setteo env
 const { Pool } = pkg;
 
 const pool = new Pool(
     {
+        
         connectionString: 'postgres://ldkxvkhnzuyofk:06c1add682312a8f434ff678aad0ff5213fa9aa4a8fbee09969b7225f58f2d23@ec2-52-86-115-245.compute-1.amazonaws.com:5432/d2r57ekn4ogjs5',
         ssl: {
             rejectUnauthorized: false
         }
-    }/*{
-    user: 'ldkxvkhnzuyofk',
-    host: 'ec2-52-86-115-245.compute-1.amazonaws.com',
-    database: 'd2r57ekn4ogjs5',
-    password: '06c1add682312a8f434ff678aad0ff5213fa9aa4a8fbee09969b7225f58f2d23',
-    port: 5432,
-  }*/)
+    })
+
+
 
 //getALLLLLLLLLLLLL
 const getAll = async () => {
@@ -28,7 +26,31 @@ const getAll = async () => {
       //  let result = await 
         let result = pool.query('SELECT * FROM dias;') 
         return result
-        /*(err, res) => {
+        
+    }
+
+    catch (err) {
+        throw err
+    }
+}
+
+export default getAll;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*(err, res) => {
 
             /*for (let row of res.rows) {
                               
@@ -39,11 +61,3 @@ const getAll = async () => {
            
            //return res
            //pool.end();
-    }
-
-    catch (err) {
-        throw err
-    }
-}
-
-export default getAll;
