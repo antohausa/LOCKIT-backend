cdac9ddabbbc42deff83b51d6aec7ced420c9463
unknown <antohausa099@gmail.com>
2022-06-24 00:11:57 -0300

    a ver

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import app from './app.js'
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.clear()
     console.log(`El servidor está inicializado en el puerto ${PORT}`);
 });
