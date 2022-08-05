18093c8297bb0eebb4e4ebf400cb09681c4334d1
renataMandelman <renatamandel@gmail.com>
2022-08-05 10:54:52 -0300

    ra

--- src/users/Services/UserService.js
+++ src/users/Services/UserService.js
@@ -42,6 +42,7 @@ export const login = async (username,contrasenia) => {
         result = await pool.query(`SELECT * FROM usuarios WHERE contrasenia='${contrasenia}' AND username='${username}'`);      
        
         rta = result.recordsets[0][0];
+        console.log(result);
         
         if (rta.length == 0 ) {
             return rta = null; 
