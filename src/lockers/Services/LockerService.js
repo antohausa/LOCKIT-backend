ba0b4fe98540a046d3ef1af9e652ed65de8db26c
renatamandelman <renatamandel@gmail.com>
2022-08-09 19:29:52 -0300

    cvx

--- src/lockers/Services/LockerService.js
+++ src/lockers/Services/LockerService.js
@@ -28,7 +28,7 @@ export const getById = async (idTienda) => {
     try {
         await pool.connect();
        // const idTienda = parseInt(request.params.idTienda)
-        let result = await pool.query(`SELECT "idLocker", "fkTienda", activo, "fkCliente" FROM lockers l INNER JOIN tiendas ti ON l."fkTienda"= ti."idTienda"  WHERE "idTienda"=${idTienda}; `)
+        let result = await pool.query(`SELECT "idLocker", "fkTienda", activo, "fkCliente" FROM lockers  WHERE "fkTienda"=${idTienda}; `)
         return result.rows;
     }
     catch (err) {
