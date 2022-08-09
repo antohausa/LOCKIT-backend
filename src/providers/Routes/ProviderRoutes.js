bfe83de26b9f8774062a36fad40c77250e6aa2cd
Renatamandelman <renatamandel@gmail.com>
2022-08-09 10:39:28 -0300

    edfe

--- src/providers/Routes/ProviderRoutes.js
+++ src/providers/Routes/ProviderRoutes.js
@@ -9,7 +9,7 @@ ProviderRouter.get("/", (req, res) => {
 
 ProviderRouter.get('/getProviders', con.getProviders)
 ProviderRouter.post('/createProvider', con.createProvider)
-ProviderRouter.post('/:idTienda', con.getById)
+ProviderRouter.get('/:idTienda', con.getById)
 
 
 export default ProviderRouter;
